8e0848eec3f7f73a97a09b866cae2260feb53b64
mohanemmadi <53083279+mohanemmadi@users.noreply.github.com>
2021-02-11 22:38:37 -0800

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'mohan',
-  password: 'mohan@123',
+  password: 'Mp@595913',
   database: 'user'
 });
 
